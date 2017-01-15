3b7d692b5722152b05637e7e8bfdd3751416ff22
Clinton D'souza <clinton_dsouza92@yahoo.com>
2017-01-15 23:53:53 +0400

    Slider

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -25,3 +25,52 @@ $ ->
     nextText: '<i class="fa fa-chevron-right fa-3x" aria-hidden="true"></i>',
     prevText: '<i class="fa fa-chevron-left fa-3x" aria-hidden="true"></i>'
   });
+  $('#timeline-container-basic').timelineMe({
+    items: [
+      {
+        type: 'smallItem',
+        label: '2006',
+        showMore: 'show more',
+        showLess: 'show less',
+        shortContent:'The Beginning'
+        fullContent: '<div class="">
+      <i class="col-md-2 fa fa-flag fa-2x" style="color: #c59a6d"></i>
+      <div class="col-md-10">
+      <h3 style="color: #c59a6d">The Beginning</h3>
+<p>Quisque ut convallis diam, id pulvinar leo. Integer consequat vulputate orci sit amet consequat</p>
+</div>
+</div>',
+
+      },
+      {
+        type: 'smallItem',
+        label: '2007',
+        showMore: 'show more',
+        showLess: 'show less',
+        shortContent:'The First Award'
+        fullContent: '<div class="">
+      <i class="col-md-2 fa fa-trophy fa-2x" style="color: #c59a6d"></i><div class="col-md-10">
+      <h3 style="color: #c59a6d">The First Award</h3>
+<p>Quisque ut convallis diam, id pulvinar leo. Integer consequat vulputate orci sit amet consequat</p>
+</div>
+
+</div>',
+
+      },
+      {
+        type: 'smallItem',
+        label: '2008',
+        showMore: 'show more',
+        showLess: 'show less',
+        shortContent:'We Have A new Office'
+        fullContent: '<div class="">
+      <i class="col-md-2 fa fa-building fa-2x" style="color: #c59a6d"></i>
+      <div class="col-md-10">
+      <h3 style="color: #c59a6d">We Have A new Office</h3>
+<p>Quisque ut convallis diam, id pulvinar leo. Integer consequat vulputate orci sit amet consequat</p>
+</div>
+</div>',
+
+      }
+    ]
+  });
--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@
       nextText: '<i class="fa fa-chevron-right fa-3x" aria-hidden="true"></i>',
       prevText: '<i class="fa fa-chevron-left fa-3x" aria-hidden="true"></i>'
     });
-    return $('.slider2').bxSlider({
+    $('.slider2').bxSlider({
       minSlides: 1,
       maxSlides: 1,
       moveSlides: 1,
@@ -18,6 +18,32 @@
       prevSelector: '#slider-prev-2',
       nextText: '<i class="fa fa-chevron-right fa-3x" aria-hidden="true"></i>',
       prevText: '<i class="fa fa-chevron-left fa-3x" aria-hidden="true"></i>'
+    });
+    return $('#timeline-container-basic').timelineMe({
+      items: [
+        {
+          type: 'smallItem',
+          label: '2006',
+          showMore: 'show more',
+          showLess: 'show less',
+          shortContent: 'The Beginning',
+          fullContent: '<div class=""> <i class="col-md-2 fa fa-flag fa-2x" style="color: #c59a6d"></i> <div class="col-md-10"> <h3 style="color: #c59a6d">The Beginning</h3> <p>Quisque ut convallis diam, id pulvinar leo. Integer consequat vulputate orci sit amet consequat</p> </div> </div>'
+        }, {
+          type: 'smallItem',
+          label: '2007',
+          showMore: 'show more',
+          showLess: 'show less',
+          shortContent: 'The First Award',
+          fullContent: '<div class=""> <i class="col-md-2 fa fa-trophy fa-2x" style="color: #c59a6d"></i><div class="col-md-10"> <h3 style="color: #c59a6d">The First Award</h3> <p>Quisque ut convallis diam, id pulvinar leo. Integer consequat vulputate orci sit amet consequat</p> </div> </div>'
+        }, {
+          type: 'smallItem',
+          label: '2008',
+          showMore: 'show more',
+          showLess: 'show less',
+          shortContent: 'We Have A new Office',
+          fullContent: '<div class=""> <i class="col-md-2 fa fa-building fa-2x" style="color: #c59a6d"></i> <div class="col-md-10"> <h3 style="color: #c59a6d">We Have A new Office</h3> <p>Quisque ut convallis diam, id pulvinar leo. Integer consequat vulputate orci sit amet consequat</p> </div> </div>'
+        }
+      ]
     });
   });
 
